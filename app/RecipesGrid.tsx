47117324f6abca667e343280eace7790857a614e
Basel <basel@astudio.ae>
2023-12-16 23:08:40 +0200

    fix 304 Not Modified

--- app/RecipesGrid.tsx
+++ app/RecipesGrid.tsx
@@ -7,7 +7,12 @@ import prisma from '@/lib/prisma';
 const RecipesGrid: React.FC = () => {
   const [recipes, setRecipes] = useState([]);
   useEffect(() => {
-    fetch('/api/recipes', { cache: 'no-cache' })
+    fetch('/api/recipes', {
+      cache: 'no-cache',
+      next: {
+        revalidate: 60,
+      },
+    })
       .then((res) => res.json())
       .then((data) => {
         setRecipes(data);
