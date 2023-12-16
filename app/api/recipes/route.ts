0fb10dfebba68032d896c6d6d3ed1d1fca23d6c7
Basel <basel@astudio.ae>
2023-12-17 00:56:40 +0200

    fix

--- app/api/recipes/route.ts
+++ app/api/recipes/route.ts
@@ -1,5 +1,6 @@
 import prisma from '../../../lib/prisma';
 
+export const dynamic = 'force-dynamic';
 export async function GET(request: Request) {
   const recipes = await prisma.recipe.findMany({
     include: {
